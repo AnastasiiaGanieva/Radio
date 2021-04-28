f8e72b61a3a0e37e7e27f7e3acee384590e77109
Anastasiya Ganieva <ganievrav@rambler.ru>
2021-04-28 20:45:43 +0400

    adding

---
+++ src/test/java/ru/netology/RadioProjectTest.java
@@ -0,0 +1,130 @@
+package ru.netology.domain;
+
+public class RadioProject {
+
+    private int maxStation = 10;
+    private int minStation = 0;
+    private int currentStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int currentVolume;
+    private boolean on;
+
+    public RadioProject() {
+    }
+
+    public RadioProject(int maxStation, boolean on) {
+        this.maxStation = maxStation;
+        this.on = on;
+    }
+
+
+    public void increaseStationNext() {
+        if (currentStation == maxStation){
+            this.currentStation = minStation;
+            return;
+        }
+        currentStation ++;
+    }
+
+    public void decreaseStationPrev() {
+        if (currentStation == minStation){
+            this.currentStation = maxStation;
+            return;
+        }
+        currentStation --;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume ++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume){
+            return;
+        }
+        currentVolume --;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+
+//    public void setMaxStation(int maxStation) {
+//        this.maxStation = maxStation;
+//    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+//    public void setMinStation(int minStation) {
+//        this.minStation = minStation;
+//    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+
+        if (currentStation > maxStation){
+            this.currentStation = maxStation;
+            return;
+        }
+
+        if (currentStation < minStation) {
+            this.currentStation = minStation;
+            return;
+        }
+
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+//    public void setMaxVolume(int maxVolume) {
+//        this.maxVolume = maxVolume;
+//    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+//    public void setMinVolume(int minVolume) {
+//        this.minVolume = minVolume;
+//    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+            return;
+        }
+
+        if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
+            return;
+        }
+
+        this.currentVolume = currentVolume;
+    }
+
+    public boolean isOn(){
+        return on;
+    }
+
+    public void setOn(boolean on){
+        this.on = on;
+    }
+
+}
